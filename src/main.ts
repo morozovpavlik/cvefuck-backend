19a17a5c3b36a8a4508ab55f0e9f616dd7ff4cce
paviel.kieruziel <paviel.kieruziel@yandex.ru>
2026-01-11 16:45:20 +0300

    fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+  app.setGlobalPrefix('v1');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
